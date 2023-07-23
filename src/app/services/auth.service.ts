8b859589cc90144fe0ca36bb9ddb28cd4462e3f2
Gaetan-B <78168196+Gaetan-B@users.noreply.github.com>
2023-07-23 20:10:11 +0200

    commit
    promise singin/up

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import firebase from "firebase/compat";
+import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,42 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  createNewUser(email: string, password: string) {
+    return new Promise<void>(
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error: any) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  signInUser(email: string, password: string) {
+    return new Promise<void>(
+      (resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error)
+          }
+        )
+      }
+    )
+  }
 }
+function reject(error: any) {
+    throw new Error('Function not implemented.');
+}
+
+function resolve() {
+    throw new Error('Function not implemented.');
+}
+
